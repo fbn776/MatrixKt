3141dbb048a9cea3296ac73250bf89093e6de803
fbn776 <fbn776@gmail.com>
2023-05-17 00:14:00 +0530

    Added toString() method

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val values = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val matrix = Matrix(values, 3, 3)
+    print(matrix)
+}
--- src/main/kotlin/Matrix.kt
+++ src/main/kotlin/Matrix.kt
@@ -4,20 +4,50 @@ data class MatrixError(val errorName: String, val hasError: Boolean, val display
     }
 }
 
-class Matrix(values: List<Number>, rows: Int, cols: Int) {
-    private val matrixSizeError = MatrixError("Matrix Size Error", false, "The size of the matrix, doesn't match the rows and columns")
+class Matrix(matrix1D: List<Number>, rows: Int, cols: Int) {
+    private val matrixSizeError =
+        MatrixError("Matrix Size Error", false, "The size of the matrix, doesn't match the rows and columns")
+
+
+    //    fun map1DTo2D(array1D: IntArray, rows: Int, columns: Int): Array<Array<Int>> {
+//        val array2D = Array(rows) { Array(columns) { 0 } }
+//
+//        for (i in 0 until rows) {
+//            for (j in 0 until columns) {
+//                array2D[i][j] = array1D[i * columns + j]
+//            }
+//        }
+//
+//        return array2D
+//    }
+
+    //Create a 2D array that `rows` rows and `cols` columns and fill each cell with 0;
+    val matrix2D = Array(rows) { Array(cols) { 0 } }
 
     init {
-        //Check if
-        if(rows * cols != values.size) {
+        //Check if the size of the matrix values is in accordance with the given rows and cols value;
+        if (rows * cols != matrix1D.size) {
             matrixSizeError.throwError()
         }
 
-        for (j in 1..cols) {
-            val column = mutableListOf<Number>()
-            for(i in 1..rows) {
-                column.add(values[i])
+//        for (i in 1..rows) {
+//            val row = mutableListOf<Number>()
+//            for (j in 1..cols) {
+//                row.add(values[i * j])
+//            }
+//        }
+    }
+
+    //A formatted matrix string;
+    override fun toString(): String {
+        var str = ""
+        for(i in matrix2D) {
+            var str_temp = ""
+            for(cell in i) {
+                str_temp += "$cell "
             }
+            str += "$str_temp\n"
         }
+        return str
     }
 }
