3e24d067b40035a29a48ab811354e334d5f880f5
fbn776 <fbn776@gmail.com>
2023-05-17 22:56:52 +0530

    Readme file + some minor changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,16 @@
+
 fun main() {
-    val values = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    val matrix = Matrix(values, 3, 3)
-    print(matrix)
+    val values1 = listOf(1, 2, 3, 4, 5, 6, 7, 8.5, 9)
+    val values2 = listOf(0, 0, 0, 0, 0, 0, 0, 0, 0)
+    val m1 = Matrix(values1, 3, 3)
+    val m2 = Matrix(values2, 3, 3)
+
+
+    val matrix1 = m1.matrix2D
+    operator fun Number.plus(other: Number) = (this.toDouble() + other.toDouble()) as Number
+    println(matrix1[0][0] + matrix1[1][2])
+    //println(m1 add m2)
+    println(m1[0, 0])
+    print(m1)
+
 }
--- src/main/kotlin/Matrix.kt
+++ src/main/kotlin/Matrix.kt
@@ -4,12 +4,12 @@ data class MatrixError(val errorName: String, val hasError: Boolean, val display
     }
 }
 
-class Matrix<T: Number>(matrix1D: List<T>, rows: Int, cols: Int) {
+class Matrix<T: Number>(val matrix1D: List<T>, rows: Int, cols: Int) {
     private val matrixSizeError =
         MatrixError("Matrix Size Error", false, "The size of the matrix, doesn't match the rows and columns")
 
     //Create a 2D array that `rows` rows and `cols` columns and fill each cell with 0;
-    val matrix2D = Array(rows) { Array<Number>(cols) { 0 } }
+    val matrix2D = Array(rows) { Array(cols) { 0 as Number} }
 
     init {
         //Check if the size of the matrix values is in accordance with the given rows and cols value;
@@ -19,11 +19,24 @@ class Matrix<T: Number>(matrix1D: List<T>, rows: Int, cols: Int) {
         //Packs the 1D matrix with the `rows` and `cols` information into a 2D matrix.
         for (i in 0 until rows) {
             for (j in 0 until cols) {
-                matrix2D[i][j] = matrix1D[i * cols + j]
+                matrix2D[i][j] = matrix1D[i * cols + j].toDouble()
             }
         }
     }
 
+    infix fun <E: Number> add(other: Matrix<E>) {
+        this.matrix2D.forEachIndexed {i, row ->
+            row.forEachIndexed { j, cell ->
+                var x = this.matrix2D[i][j]
+            }
+        }
+    }
+    companion object {
+    }
+
+
+
+    operator fun get(i: Int, j: Int) = matrix2D[i][j]
     //A formatted matrix string;
     override fun toString(): String {
         var str = ""
